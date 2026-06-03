d8eaf487b527966cf597538a532c5c068c9daf0b
lianglixia1234 <161739208+lianglixia1234@users.noreply.github.com>
2026-06-03 16:18:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -113,25 +113,8 @@ if uploaded_file is not None:
     ax.scatter(x, means, color="black", s=70, zorder=5)
 
     
-    # min/max
+    # 最大最小值
     for i in range(len(conditions)):
-
-        # mean数值
-        ax.text(
-            i + 0.08,      # 向右偏移
-            mean,          # 与均值点同高
-            f"Mean={mean:.2f}",
-            ha="left",
-            va="center",
-            fontsize=10,
-            fontweight="bold",
-            bbox=dict(
-                facecolor="white",
-                alpha=0.8,
-                edgecolor="none"
-            )
-        )
-
         
         
         ax.hlines(
